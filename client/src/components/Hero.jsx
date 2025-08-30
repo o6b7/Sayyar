9675e452cd97c3671db5a972e8f8b97440cd4234
Qusai Abdullah <98080234+o6b7@users.noreply.github.com>
2025-08-30 11:15:39 +0300

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -77,7 +77,7 @@ const Hero = () => {
           <div className="flex items-end w-full md:w-auto">
             <button 
               type="submit"
-              className="flex items-center justify-center gap-2 px-8 py-3 w-full md:w-auto bg-primary hover:bg-primary-dull text-white rounded-lg shadow-md hover:scale-105 transition"
+              className="flex items-center justify-center gap-2 px-8 py-3 w-full md:w-auto mb-1 bg-primary hover:bg-primary-dull text-white rounded-lg shadow-md hover:scale-105 transition"
             >
               <Search className="w-5 h-5" />
               بحث
